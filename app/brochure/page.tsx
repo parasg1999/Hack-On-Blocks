3092b3b88e8b4d499ea788ffd9e6b9731b647c28
Paras Gupta <parasg1999@gmail.com>
2024-04-24 18:38:01 +0530

    brochure embed

---
+++ app/brochure/page.tsx
@@ -0,0 +1,11 @@
+export default function Brochure() {
+  return (
+    <iframe
+      src="https://drive.google.com/file/d/1NtMxgRzQjanGdCTKuv6fqCPmwzASPSxX/preview"
+      className="w-screen h-screen"
+      //   width="640"
+      //   height="480"
+      allow="autoplay"
+    ></iframe>
+  );
+}
