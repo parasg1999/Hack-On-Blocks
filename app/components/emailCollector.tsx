7a5f47be600ce4243c449817ae978551cf9b49c7
Paras Gupta <parasg1999@gmail.com>
2024-05-01 21:45:32 +0530

    fix: faqs, socials

--- app/components/emailCollector.tsx
+++ app/components/emailCollector.tsx
@@ -38,21 +38,21 @@ export default function EmailCollector() {
   };
 
   return (
-    <div className="flex flex-col items-center gap-y-[1rem] w-[90%]">
-      <div className="flex flex-col md:flex-row gap-x-[2rem] items-center gap-y-10">
-        <div className="flex flex-row bg-[#343935] font-satoshi rounded-md overflow-hidden w-fit z-10">
-          <div className="bg-white text-black font-bold px-[1.5rem] py-[1rem] rounded-md text-[1.25]rem md:text-[1.5rem] flex flex items-center ">
+    <div className="flex flex-col items-center gap-y-[1rem] w-[100%]">
+      <div className="flex flex-col md:flex-row gap-x-[2rem] items-center gap-y-8">
+        <div className="flex flex-row bg-[#343935] font-satoshi rounded-md overflow-hidden md:w-fit z-10">
+          <div className="bg-white text-black font-bold px-4 md:px-[1.5rem] py-[1rem] rounded-md text-[1.25]rem md:text-[1.5rem] flex flex items-center ">
             <div className="h-[2rem] w-[2rem] bg-[#45DB5D40] rounded-full flex items-center justify-center">
               <span className="h-[0.9rem] w-[0.9rem] bg-[#45DB5D] border rounded-full"></span>
             </div>
             <span className="ml-[0.2rem]">online</span>
           </div>
-          <div className="font-mono px-[1rem] text-[1.25]rem md:text-[1.25rem] text-white flex items-center">
+          <div className="font-mono px-[0.75rem] md:px-[1rem] text-[1rem] md:text-[1.25rem] text-white flex items-center">
             24 JUN - 24 JUL ‘24
           </div>
         </div>
         <div className="flex flex-col md:flex-row bg-[#80E93D] font-satoshi rounded-md overflow-hidden w-fit z-10">
-          <div className="text-black font-bold px-[1.5rem] py-[1rem] rounded-md text-[1.25]rem md:text-[1.5rem] flex items-center ">
+          <div className="text-black font-bold px-[1.5rem] py-[1rem] rounded-md text-[1rem] md:text-[1.5rem] flex items-center ">
             register
             <Image
               src={arrowImage}
