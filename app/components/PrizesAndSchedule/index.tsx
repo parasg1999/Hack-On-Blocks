c61b55c644ca42efb63d677c5ec839ebf94e1277
Paras Gupta <parasg1999@gmail.com>
2024-05-01 21:47:28 +0530

    fix: prizes section width

--- app/components/PrizesAndSchedule/index.tsx
+++ app/components/PrizesAndSchedule/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function PrizesAndSchedule() {
   return (
-    <div className="flex flex-col items-center mt-12">
+    <div className="flex flex-col items-center mt-12 max-w-[1280px] w-[90%] m-auto">
       <h1 className="text-2xl md:text-6xl text-center font-quinque text-[#46424D] mt-8 text-center">
         PRIZES &amp; SCHEDULE
       </h1>
