cd01918486766b6df8162d3da37edb745ee7c97d
Paras Gupta <parasg1999@gmail.com>
2024-05-25 11:37:49 +0530

    add: twitter script

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { JetBrains_Mono } from "next/font/google";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import { Metadata } from "next";
+import Script from "next/script";
 
 const satoshi = localFont({
   src: [
@@ -73,6 +74,12 @@ export default function RootLayout({
     >
       <body>{children}</body>
       <GoogleAnalytics gaId="UA-162071861-1" />
+      <Script id="twitter-tracking">
+        {`!function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+      },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+      a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+      twq('config','o5qcw');`}
+      </Script>
     </html>
   );
 }
