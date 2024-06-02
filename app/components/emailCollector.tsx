e67f184ec6ba420b1f3357a204df09f9cde45ae0
Paras Gupta <parasg1999@gmail.com>
2024-06-02 14:59:06 +0530

    ripple animation

--- app/components/emailCollector.tsx
+++ app/components/emailCollector.tsx
@@ -7,8 +7,10 @@ export default function EmailCollector() {
       <div className="flex flex-col md:flex-row gap-x-[2rem] items-center gap-y-8">
         <div className="flex flex-row bg-[#343935] font-satoshi rounded-md overflow-hidden md:w-fit z-10">
           <div className="bg-white text-black font-bold px-4 md:px-[1.5rem] py-[1rem] rounded-md text-[1.25]rem md:text-[1.5rem] flex flex items-center ">
-            <div className="h-[2rem] w-[2rem] bg-[#45DB5D40] rounded-full flex items-center justify-center">
-              <span className="h-[0.9rem] w-[0.9rem] bg-[#45DB5D] border rounded-full"></span>
+            <div className="h-[2rem] w-[2rem] bg-white rounded-full flex items-center justify-center relative">
+              <div className="absolute ripple-border-outside rounded-full"></div>
+              <div className="absolute ripple-border-inside rounded-full"></div>
+              <span className="h-[0.9rem] w-[0.9rem] bg-[#45DB5D] rounded-full"></span>
             </div>
             <span className="ml-[0.2rem]">Online</span>
           </div>
