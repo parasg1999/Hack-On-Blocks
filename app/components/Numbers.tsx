ee9a2b256de726226fb0af4cd057e4183b977c54
Paras Gupta <parasg1999@gmail.com>
2024-06-20 17:24:30 +0530

    fix: meetups

--- app/components/Numbers.tsx
+++ app/components/Numbers.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import DEL from "@/app/assets/images/meetups/Delhi.png";
 import AMD from "@/app/assets/images/meetups/Ahmedabad.png";
 import BLR from "@/app/assets/images/meetups/Bangalore.png";
@@ -12,7 +14,6 @@ import label1 from "@/app/assets/images/label-1.svg";
 import label2 from "@/app/assets/images/label-2.svg";
 import date from "@/app/assets/images/icons/date.svg";
 import link from "@/app/assets/images/icons/link.svg";
-import location from "@/app/assets/images/icons/location.svg";
 import Image from "next/image";
 
 const numbers = [
@@ -22,7 +23,7 @@ const numbers = [
     heading: "Jaipur",
     date: "09 June",
     location: "DEL",
-    link: "https://lu.ma/HackOnDEL",
+    link: "lu.ma/HackOnDEL",
     class: "TBD",
     parentClass: "pl-0 pt-0 pr-2 pb-2 md:pr-4 md:pb-4",
   },
@@ -32,7 +33,7 @@ const numbers = [
     heading: "bengaluru",
     date: "15 June",
     location: "Razorpay",
-    link: "https://lu.ma/HackOnBLR",
+    link: "lu.ma/HackOnBLR",
     class: "border rounded-3xl bg-[#FAFAFA] border-[#00000033]",
     parentClass:
       "border-l border-dashed border-[#D4D4D4] pr-0 md:pr-4 pl-2 pt-0 pb-2 md:pb-4 md:pl-4 md:pt-0",
@@ -43,7 +44,7 @@ const numbers = [
     heading: "Mumbai",
     date: "15 June",
     location: "Microsoft",
-    link: "https://lu.ma/HackOnMUM",
+    link: "lu.ma/HackOnMUM",
     class:
       "bg-[#FAFAFA] border border-[#00000033] md:border-0 rounded-3xl md:bg-transparent ",
     parentClass:
@@ -55,7 +56,7 @@ const numbers = [
     heading: "Pune",
     date: "16 June",
     location: "Urban Nomads",
-    link: "https://lu.ma/HackOnPUNE",
+    link: "lu.ma/HackOnPUNE",
     class:
       "bg-transparent md:border border-[#00000033] rounded-3xl md:bg-[#FAFAFA]",
     parentClass:
@@ -67,7 +68,7 @@ const numbers = [
     heading: "Hyderabad",
     date: "22 June",
     location: "Microsoft",
-    link: "https://lu.ma/HackOnHYD",
+    link: "lu.ma/HackOnHYD",
     class:
       "bg-transparent md:border border-[#00000033]  rounded-3xl md:bg-[#FAFAFA]",
     parentClass:
@@ -79,7 +80,7 @@ const numbers = [
     location: "TBD",
     date: "22 June",
     heading: "Kochi",
-    link: "https://lu.ma/HackOnCOK",
+    link: "lu.ma/HackOnCOK",
     class:
       "bg-[#FAFAFA] border md:border-0 border-[#00000033] rounded-3xl md:bg-transparent",
     parentClass:
@@ -91,7 +92,7 @@ const numbers = [
     heading: "Kolkata",
     date: "23 June",
     location: "TBD",
-    link: "https://lu.ma/HackOnCCU",
+    link: "lu.ma/HackOnCCU",
     class: "border border-[#00000033] rounded-3xl bg-[#FAFAFA]",
     parentClass:
       "border-l-none md:border-l border-t border-dashed border-[#D4D4D4] pl-0 md:pl-4 pr-2 pt-2 md:pr-4 pb-0 md:pt-4",
@@ -102,7 +103,7 @@ const numbers = [
     heading: "Chennai",
     date: "23 June",
     location: "TBD",
-    link: "https://lu.ma/HackOnCHE",
+    link: "lu.ma/HackOnCHE",
     class:
       "bg-transparent border-none md:border border-[#00000033] rounded-3xl md:bg-transparent",
     parentClass:
@@ -114,7 +115,7 @@ const numbers = [
     heading: "Delhi",
     date: "29 June",
     location: "Microsoft",
-    link: "https://lu.ma/HackOnDEL",
+    link: "lu.ma/HackOnDEL",
     class:
       "bg-transparent border-none md:border border-[#00000033] rounded-3xl md:bg-transparent",
     parentClass:
@@ -126,7 +127,7 @@ const numbers = [
     heading: "Ahmedabad",
     date: "29 June",
     location: "TBD",
-    link: "https://lu.ma/HackOnAMD",
+    link: "lu.ma/HackOnAMD",
     class:
       "bg-transparent border-none md:border border-[#00000033] rounded-3xl md:bg-transparent",
     parentClass:
@@ -140,22 +141,30 @@ export default function Numbers() {
       id="numbers"
       className="flex flex-col justify-center items-center mt-8 md:mt-2 max-w-[1280px] m-auto w-[90%]"
     >
-      <h1 className="text-2xl md:text-6xl text-center font-quinque text-[#46424D] mt-8 text-center !leading-relaxed">
+      {/* <h1 className="text-2xl md:text-6xl text-center font-quinque text-[#46424D] mt-8 text-center !leading-relaxed">
         MEETUPS
-      </h1>
+      </h1> */}
 
       <h1 className="text-2xl md:text-6xl text-center font-quinque text-[#46424D] mt-8 text-center !leading-relaxed">
         BUILDERS CLUB
       </h1>
       <div className="grid grid-cols-2 md:grid-cols-5 gap-x-4 gap-y-4 mt-8 w-full mx-auto mb-24 mt-24">
         {numbers.map((s, i) => (
-          <div key={i}>
+          <div
+            key={i}
+            onClick={() => window.open(`https://${s.link}`, "_blank")}
+          >
             <div
               className={
-                "flex flex-col font-satoshi gap-y-3 px-2 py-4 md:px-4 md:py-8 items-center justify-between h-full bg-white rounded-lg md:rounded-2xl"
+                "flex flex-col font-satoshi gap-y-3 px-2 py-4 md:px-4 md:py-8 items-center justify-between h-full bg-white rounded-lg md:rounded-2xl relative"
               }
             >
-              <Image src={s.img} width={128} height={128} alt="del" />
+              <Image
+                src={s.img}
+                alt="del"
+                className=" h-[64px] w-[64px] md:h-[128px] md:w-[128px]"
+              />
+              <div className="absolute top-0 w-full min-h-[64px] md:min-h-[128px] bg-black z-20 mt-4 md:mt-8 transparent-gradient"></div>
               <a
                 style={{
                   backgroundImage: `url(${s.label.src})`,
@@ -164,22 +173,22 @@ export default function Numbers() {
                   backgroundPosition: "left center",
                   width: "fit-content",
                 }}
-                className="uppercase text-[#FCFDFE] px-4 md:px-8 py-2 font-semibold tracking-widest min-w-[80%] flex flex-row justify-center text-xs md:text-lg"
+                className="uppercase text-[#FCFDFE] px-4 md:px-8 py-2 font-semibold tracking-widest min-w-[80%] flex flex-row justify-center text-xs md:text-lg mt-[-0.75rem]"
               >
                 {s.heading}
               </a>
-              <div className="px-2 md:px-4 gap-x-2 flex flex-row font-semibold text-sm md:text-lg self-start align-center">
+              <div className="px-2 md:px-2 gap-x-2 flex flex-row font-semibold text-sm md:text-lg self-start align-center">
                 <Image src={date} alt="link" width="16" />
                 <span>{s.date}</span>
               </div>
-              <div className="px-2 md:px-4 gap-x-2 flex flex-row font-semibold text-sm md:text-lg self-start align-center">
+              {/* <div className="px-2 md:px-2 gap-x-2 flex flex-row font-semibold text-sm md:text-lg self-start align-center">
                 <Image src={location} alt="link" width="16" />
                 <span>{s.location}</span>
-              </div>
-              {/* <div className="gap-x-2 flex flex-row font-regular text-sm md:text-lg self-start align-center">
+              </div> */}
+              <div className="px-2 md:px-2 gap-x-2 flex flex-row font-semibold text-sm md:text-lg self-start align-center">
                 <Image src={link} alt="link" width="16" />
                 <span>{s.link}</span>
-              </div> */}
+              </div>
             </div>
           </div>
         ))}
