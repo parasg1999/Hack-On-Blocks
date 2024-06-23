f88ca3f2fadf873f9065bbf96c4846402b4b210b
Paras Gupta <parasg1999@gmail.com>
2024-06-23 19:20:55 +0530

    fix: ready for launch

--- app/components/Prizes/index.tsx
+++ app/components/Prizes/index.tsx
@@ -1,48 +1,77 @@
+"use client";
+
 import Image from "next/image";
-import Heading from "../Heading";
-import styles from "./prizes.module.scss";
+import dinero from "@/app/assets/images/dinero.svg";
 
-import filecoin from "../../assets/images/sponsors/dummy.svg";
+import googleCloud from "../../assets/images/prev-partners/google-cloud.svg";
+import microsoft from "../../assets/images/prev-partners/microsoft.svg";
+import polygon from "../../assets/images/partners/polygon.png";
+import fbi from "../../assets/images/partners/fbi.png";
+import solana from "../../assets/images/prev-partners/solana.svg";
+import wazirx from "../../assets/images/prev-partners/wazirx.svg";
+import github from "../../assets/images/prev-partners/github.svg";
+import sui from "../../assets/images/prev-partners/sui.svg";
 import SectionSeparator from "../SectionSeparator";
+import { useEffect, useState } from "react";
 
-export default function Prizes() {
+export default function Partners() {
+  const [partners, setPartners] = useState<any[]>([]);
+
+  useEffect(() => {
+    setPartners([
+      {
+        image: polygon,
+        amount: "$10,000",
+        link: "https://hackonblocks.devfolio.co/prizes?partner=Polygon",
+      },
+      {
+        image: fbi,
+        amount: "$5,000",
+        link: "https://hackonblocks.devfolio.co/prizes?partner=Farcaster+Builders+India",
+        classes: "bg-[#1A0149] text-white",
+      },
+    ]);
+  }, []);
+
   return (
     <div
-      id="prizes"
-      className={`${styles.prizes} flex flex-col justify-center items-center`}
+      className={`flex flex-col justify-center items-center mt-8 md:mt-2 max-w-[1280px] m-auto w-[90%]`}
+      id="partners"
     >
-      <SectionSeparator name="prizes" />
-      <h1 className="text-2xl md:text-7xl text-center font-quinque">
-        $125,000
+      <Image
+        src={dinero}
+        className="w-[30rem] mb-[-4rem] saturate-0 hover:saturate-100 transition-all"
+        alt="arrow"
+      />
+      <h1 className="text-xl md:text-6xl text-center font-quinque z-10 pointer-events-none !leading-relaxed">
+        $15,000
       </h1>
-      <h3 className="text-xl mt-4 font-satoshi font-bold md:text-4xl md:mt-8">
-        available in prizes{" "}
-      </h3>
+      <h1 className="text-xl md:text-[2.5rem] text-center font-quinque z-10 pointer-events-none !leading-relaxed">
+        PRIZE POOL
+      </h1>
       <div
         id="cards"
-        className="font-satoshi grid grid-cols-1 md:grid-cols-3 gap-x-4 gap-y-4 w-full mt-8 md:mt-20"
+        className="font-satoshi grid grid-cols-2 md:grid-cols-12 gap-x-4 gap-y-4 w-full mt-8 md:mt-20 z-10"
       >
-        {[...Array(9).keys()].map((_, idx) => (
+        {partners.map((p, idx) => (
           <div
-            className={`${styles.card} flex rounded-2xl px-6 py-4`}
+            className={
+              `bg-[#ffffffA6] cursor-pointer col-span-2 py-4 md:col-span-6 md:py-8 flex flex-col rounded-lg md:rounded-2xl overflow-hidden justify-between items-center gap-y-4 ` +
+              p.classes
+            }
             key={idx}
           >
-            <Image src={filecoin} width={56} alt="filecoin" />
-            <span className="ml-4 flex flex-col justify-evenly">
-              <div className={`${styles.description} font-bold text-xl `}>
-                Filecoin 🛸 FVM Spaceships
-              </div>
-              <div className="font-mono font-w-500 text-base opacity-80 mt-1.5">
-                $30000
-              </div>
-            </span>
+            <Image
+              src={p.image}
+              alt="filecoin"
+              height={80}
+              className={`${"max-w-[7rem] max-h-[5rem] md:max-w-[14rem] md:max-h-[10rem]"} `}
+            />
+            <h1 className="text-xl md:text-4xl text-center font-quinque z-10 pointer-events-none !leading-relaxed">
+              {p.amount}
+            </h1>
           </div>
         ))}
-      </div>
-      <div
-        className={`${styles.button} mt-6 md:mt-14 font-satoshi font-bold px-4 py-2 rounded-xl text-xl`}
-      >
-        see prize details
       </div>
     </div>
   );
